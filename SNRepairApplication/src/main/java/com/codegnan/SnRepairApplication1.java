65d168847dd0bac2dc1e4b847da34ec75941ca83
akkiadepu <akki.adepu@gmail.com>
2024-11-14 17:05:24 +0530

    last controller

--- SNRepairApplication/src/main/java/com/codegnan/SnRepairApplication1.java
+++ SNRepairApplication/src/main/java/com/codegnan/SnRepairApplication1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SnRepairApplication {
+public class SnRepairApplication1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SnRepairApplication.class, args);
+		SpringApplication.run(SnRepairApplication1.class, args);
 	}
 
 }
